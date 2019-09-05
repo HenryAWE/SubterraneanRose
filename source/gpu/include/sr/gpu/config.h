92c8dbd89b29c98802e8c6e31f246ae719030c69
AWE Henry <mouth11or12@outlook.com>
2019-09-05 14:17:06 +0000

    Added config files for GPU, editor and main module

---
+++ source/gpu/include/sr/gpu/config.h
@@ -0,0 +1,11 @@
+/**
+ * @file config.h
+ * @author HenryAWE
+ * @brief Config file of GPU module
+ */
+
+#ifndef SROSE_GPU_config_h_
+#define SROSE_GPU_config_h_
+
+
+#endif
---
+++ source/srose/include/sr/srose/config.h
@@ -0,0 +1,11 @@
+/**
+ * @file config.h
+ * @author HenryAWE
+ * @brief Config file of the main module
+ */
+
+#ifndef SROSE_SROSE_config_h_
+#define SROSE_SROSE_config_h_
+
+
+#endif
