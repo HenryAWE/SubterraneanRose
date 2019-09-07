fbd9756aa198bf407eca9b49eb8f926ef976ab0c
AWE Henry <mouth11or12@outlook.com>
2019-09-07 20:42:52 +0800

    PLAYER: Program entry on windows desktop

--- source/player/src/entry_windows.c
+++ source/player/src/entry_windows.c
@@ -13,12 +13,28 @@
 #ifdef _WIN32
 int entry_windows(int argc, char* argv[])
 {
-    if(SR_SYS_InitSDL() != 0)
+    if(SR_SYS_InitSDL(SDL_TRUE) != 0)
     {
-        return -1;
+        return EXIT_FAILURE;
     }
+    else
+    {
+        at_quick_exit(&SR_SYS_QuitSDL); // 
+    }
+    
 
     SR_WM_display* display = SR_WM_CreateDisplay("Subterranean Rose", 0);
+    if(!display)
+    {
+        SDL_ShowSimpleMessageBox(
+            SDL_MESSAGEBOX_ERROR,
+            "SR_WM_CreateDisplay() failed",
+            "See log for detailed information",
+            NULL
+        );
+        SR_SYS_QuitSDL();
+        return EXIT_FAILURE;
+    }
 
     int loop = SDL_TRUE;
     SDL_Event event;
@@ -38,6 +54,7 @@ int entry_windows(int argc, char* argv[])
     }
 
     SR_WM_DestroyDisplay(display);
+    SR_SYS_QuitSDL();
     return EXIT_SUCCESS;
 }
 #endif
