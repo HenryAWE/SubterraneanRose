b1d7f5cb70fa1feed37fea42f853d356394d8d95
AWE Henry <mouth11or12@outlook.com>
2019-09-07 13:52:57 +0800

    SYS: Added SDL2-mixer

--- source/sys/src/init.c
+++ source/sys/src/init.c
@@ -6,23 +6,48 @@
 
 #include <sr/sys/init.h>
 #include <SDL.h>
+#include <SDL_mixer.h>
 
 
-int SRSCALL SR_SYS_InitSDL()
+int SRSCALL SR_SYS_InitSDL(int msgbox_on_err)
 {
     if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
         SDL_LogError(
             SDL_LOG_CATEGORY_APPLICATION,
-            "SDL_Init() failed: %s",
+            "[SYS] SDL_Init() failed: %s",
             SDL_GetError()
         );
+        if(msgbox_on_err)
+        {
+            SDL_ShowSimpleMessageBox(
+                SDL_MESSAGEBOX_ERROR,
+                "SDL_Init() failed",
+                SDL_GetError(),
+                NULL
+            );
+        }
 
         return 1;
     }
-
-    
-    
+    if (Mix_Init(MIX_INIT_FLAC | MIX_INIT_MP3 | MIX_INIT_OGG) == 0)
+    {
+        SDL_LogError(
+            SDL_LOG_CATEGORY_APPLICATION,
+            "[SYS] Mix_Init() failed: %s",
+            SDL_GetError()
+        );
+        if(msgbox_on_err)
+        {
+            SDL_ShowSimpleMessageBox(
+                SDL_MESSAGEBOX_ERROR,
+                "Mix_Init() failed",
+                SDL_GetError(),
+                NULL
+            );
+        }
+        return 1;
+    }
 
     return 0;
 }
