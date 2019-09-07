f78436f9edf7922ac9d8282e9658520e0899a1c6
AWE Henry <mouth11or12@outlook.com>
2019-09-07 21:08:40 +0800

    Change the location of main loop

--- source/main.c
+++ source/main.c
@@ -4,54 +4,12 @@
  * @brief Program entry
  */
 
-#include <stddef.h>
-#include <stdlib.h>
-#include <SDL.h>
-#include <sr/wm/display.h>
+#include <sr/player/entry.h>
 
 
-int main(int argc, char* argv[])
+/*Program entry */
+int SDL_main(int argc, char* argv[])
 {
-	if(SDL_Init(SDL_INIT_VIDEO) < 0)
-	{ // Init failed
-		SDL_ShowSimpleMessageBox(
-			SDL_MESSAGEBOX_ERROR,
-			"SDL_Init() failed",
-			SDL_GetError(),
-			NULL
-			);
-
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		atexit(&SDL_Quit);
-	}
-
-	SR_WM_display* dis = SR_WM_CreateDisplay("Subterranean Rose", 0);
-
-	int loop = SDL_TRUE;
-	SDL_Event event;
-	SDL_zero(event);
-
-	while(loop)
-	{
-		while(SDL_PollEvent(&event))
-		{
-			switch(event.type)
-			{
-			case SDL_QUIT:
-				loop = SDL_FALSE;
-			}
-		}
-
-		SDL_SetRenderDrawColor(dis->renderer, 255, 255, 255, 255);
-		SDL_RenderClear(dis->renderer);
-
-		SDL_RenderPresent(dis->renderer);
-	}
-
-	SR_WM_DestroyDisplay(dis);
-
-	return EXIT_SUCCESS;
+    //TODO: Handling other platform
+    return entry_windows(argc, argv);
 }
