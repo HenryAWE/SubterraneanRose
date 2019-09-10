34f62e2b1eb9b48eb176a0ec73ff5b0cbb09c3da
AWE Henry <mouth11or12@outlook.com>
2019-09-10 04:34:04 +0000

    Temporarily remove SDL2-mixer

--- source/sys/src/init.c
+++ source/sys/src/init.c
@@ -6,7 +6,8 @@
 
 #include <sr/sys/init.h>
 #include <SDL.h>
-#include <SDL_mixer.h>
+// TODO: Restore in future
+// #include <SDL_mixer.h>
 
 
 int SRSCALL SR_SYS_InitSDL(int msgbox_on_err)
@@ -30,32 +31,34 @@ int SRSCALL SR_SYS_InitSDL(int msgbox_on_err)
 
         return 1;
     }
-    if (Mix_Init(MIX_INIT_FLAC | MIX_INIT_MP3 | MIX_INIT_OGG) == 0)
-    {
-        SDL_LogError(
-            SDL_LOG_CATEGORY_APPLICATION,
-            "[SYS] Mix_Init() failed: %s",
-            SDL_GetError()
-        );
-        if(msgbox_on_err)
-        {
-            SDL_ShowSimpleMessageBox(
-                SDL_MESSAGEBOX_ERROR,
-                "Mix_Init() failed",
-                SDL_GetError(),
-                NULL
-            );
-        }
-        SDL_Quit();
+    // TODO: Restore in future
+    // if (Mix_Init(MIX_INIT_FLAC | MIX_INIT_MP3 | MIX_INIT_OGG) == 0)
+    // {
+    //     SDL_LogError(
+    //         SDL_LOG_CATEGORY_APPLICATION,
+    //         "[SYS] Mix_Init() failed: %s",
+    //         SDL_GetError()
+    //     );
+    //     if(msgbox_on_err)
+    //     {
+    //         SDL_ShowSimpleMessageBox(
+    //             SDL_MESSAGEBOX_ERROR,
+    //             "Mix_Init() failed",
+    //             SDL_GetError(),
+    //             NULL
+    //         );
+    //     }
+    //     SDL_Quit();
 
-        return 1;
-    }
+    //     return 1;
+    // }
 
     return 0;
 }
 
 void SRSCALL SR_SYS_QuitSDL(void)
 {
-    Mix_Quit();
+    // TODO: Restore in future
+    // Mix_Quit();
     SDL_Quit();
 }
