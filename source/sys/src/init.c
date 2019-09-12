0591534cfd62e88aced27856523712cc705f2ffc
AWE Henry <mouth11or12@outlook.com>
2019-09-12 22:36:48 +0800

    SYS: Restore SDL2-mixer

--- source/sys/src/init.c
+++ source/sys/src/init.c
@@ -6,12 +6,12 @@
 
 #include <sr/sys/init.h>
 #include <SDL.h>
-// TODO: Restore in future
-// #include <SDL_mixer.h>
+#include <SDL_mixer.h>
 
 
 int SRSCALL SR_SYS_InitSDL(int msgbox_on_err)
 {
+    #define VERSION_NUMBER_2_ARG(x) ((x).major),((x).minor),((x).patch)
     if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
         SDL_LogError(
@@ -31,34 +31,60 @@ int SRSCALL SR_SYS_InitSDL(int msgbox_on_err)
 
         return 1;
     }
-    // TODO: Restore in future
-    // if (Mix_Init(MIX_INIT_FLAC | MIX_INIT_MP3 | MIX_INIT_OGG) == 0)
-    // {
-    //     SDL_LogError(
-    //         SDL_LOG_CATEGORY_APPLICATION,
-    //         "[SYS] Mix_Init() failed: %s",
-    //         SDL_GetError()
-    //     );
-    //     if(msgbox_on_err)
-    //     {
-    //         SDL_ShowSimpleMessageBox(
-    //             SDL_MESSAGEBOX_ERROR,
-    //             "Mix_Init() failed",
-    //             SDL_GetError(),
-    //             NULL
-    //         );
-    //     }
-    //     SDL_Quit();
+    SDL_version sdl_version;
+    SDL_version sdl_version_linked;
+    SDL_VERSION(&sdl_version);
+    SDL_GetVersion(&sdl_version_linked);;
+    SDL_LogInfo(
+        SDL_LOG_CATEGORY_APPLICATION,
+        "[SYS] SDL_Init() success\n"
+        "SDL Info:\n"
+        "  Version %d.%d.%d\n"
+        "  Runtime Version: %d.%d.%d",
+        VERSION_NUMBER_2_ARG(sdl_version),
+        VERSION_NUMBER_2_ARG(sdl_version_linked)
+    );
 
-    //     return 1;
-    // }
+    if (Mix_Init(MIX_INIT_FLAC | MIX_INIT_MP3 | MIX_INIT_OGG) == 0)
+    {
+        SDL_LogError(
+            SDL_LOG_CATEGORY_APPLICATION,
+            "[SYS] Mix_Init() failed: %s",
+            SDL_GetError()
+        );
+        if(msgbox_on_err)
+        {
+            SDL_ShowSimpleMessageBox(
+                SDL_MESSAGEBOX_ERROR,
+                "Mix_Init() failed",
+                SDL_GetError(),
+                NULL
+            );
+        }
+        SDL_Quit();
+
+        return 1;
+    }
+
+    SDL_version mixer_version;
+    const SDL_version* mixer_version_linked;
+    MIX_VERSION(&mixer_version);
+    mixer_version_linked = Mix_Linked_Version();
+    SDL_LogInfo(
+        SDL_LOG_CATEGORY_APPLICATION,
+        "[SYS] Mix_Init() success\n"
+        "Mixer Info:\n"
+        "  Version %d.%d.%d\n"
+        "  Runtime Version: %d.%d.%d",
+        VERSION_NUMBER_2_ARG(mixer_version),
+        VERSION_NUMBER_2_ARG(*mixer_version_linked)
+    );
 
     return 0;
 }
 
 void SRSCALL SR_SYS_QuitSDL(void)
 {
-    // TODO: Restore in future
-    // Mix_Quit();
+    Mix_Quit();
     SDL_Quit();
 }
