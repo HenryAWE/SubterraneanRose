83abc83556410b71dfd95f4a73761cc131034e3d
AWE Henry <mouth11or12@outlook.com>
2019-09-20 22:26:50 +0800

    UI: Clear frame correctly

--- source/ui/src/main_loop.cpp
+++ source/ui/src/main_loop.cpp
@@ -15,10 +15,15 @@ int SRSCALL main_loop(SR_WM_display* display)
     int loop = SDL_TRUE;
     while(loop)
     {
+        /*Event processing*/
         loop = SR_WM_ProcessEvent();
         SR_WM_NewFrame();
         ImGui::ShowDemoWindow();
         SR_WM_EndFrame();
+
+        /*Rendering */
+        SDL_SetRenderDrawColor(display->renderer, 0, 0, 0, 255);
+        SDL_RenderClear(display->renderer);
         SR_WM_RenderFrame();
         SDL_RenderPresent(display->renderer);
     }
