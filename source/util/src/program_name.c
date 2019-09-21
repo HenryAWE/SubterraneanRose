811bc2a665fc57385934d64e4c1f1a4a5a31c235
AWE Henry <mouth11or12@outlook.com>
2019-09-21 16:06:27 +0800

    UTIL: Init module

---
+++ source/util/src/program_name.c
@@ -0,0 +1,37 @@
+/**
+ * @file program_name.c
+ * @author HenryAWE
+ * @brief Get the full path to the program executable
+ */
+
+#include <sr/util/program_name.h>
+#include <string.h>
+#include <SDL_platform.h>
+#include <SDL_syswm.h>
+#ifdef __LINUX__
+#   include <unistd.h>
+#endif
+
+
+const char* SRSCALL SR_UTIL_GetProgramName()
+{
+#ifdef __WINDOWS__
+    static CHAR buffer[MAX_PATH];
+    GetModuleFileNameA(NULL, buffer, MAX_PATH);
+
+    return buffer;
+#elif defined __LINUX__
+    static char buffer[512];
+    int ret = readlink("/proc/self/exe", buffer, 512);
+    if(ret<0||ret>512)
+        return ""; // failed
+
+    return buffer;
+#else
+    static char buffer[512];
+    strcpy(buffer, getcwd());
+    strcat(buffer, "srose-exec");
+
+    return buffer;
+#endif
+}
