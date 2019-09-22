a06f3e6da245b3bc011e1f099eb7b58b5a541571
AWE Henry <mouth11or12@outlook.com>
2019-09-22 16:46:51 +0800

    UI-MainMenuBar: Added File, Window and About menu

--- source/ui/src/main_loop.cpp
+++ source/ui/src/main_loop.cpp
@@ -5,7 +5,7 @@
  */
 
 #include "main_loop.hpp"
-#include <stdlib.h>
+#include <cstdlib>
 #include <sr/wm/event.h>
 #include <sr/sys/version_info.h>
 #include <imgui.h>
@@ -13,6 +13,9 @@
 
 int SRSCALL main_loop(SR_WM_display* display)
 {
+    bool show_about = false;
+    bool show_imgui_demo = false;
+
     int loop = SDL_TRUE;
     while(loop)
     {
@@ -24,14 +27,75 @@ int SRSCALL main_loop(SR_WM_display* display)
         if(ImGui::BeginMainMenuBar())
         {
             ImGui::TextColored(
+                {0.01f, 0.549f, 0.85f, 1},
+                "SR - %d.%d.%d",
+                SR_VERSION_MAJOR, SR_VERSION_MINOR, SR_VERSION_PATCH
+            );
+            ImGui::Separator();
+
+            if(ImGui::BeginMenu("File##SR"))
+            {
+                if(ImGui::Checkbox("Show ImGui Demo", &show_imgui_demo)){}
+                ImGui::Separator();
+
+                if(ImGui::MenuItem("Quit"))
+                    loop = SDL_FALSE;
+
+                ImGui::EndMenu();
+            }
+            if(ImGui::BeginMenu("Window##SR"))
+            {
+                if(ImGui::MenuItem("Toggle full screen"))
+                {
+                    SDL_SetWindowFullscreen(
+                        display->win,
+                        !(SDL_GetWindowFlags(display->win)&SDL_WINDOW_FULLSCREEN)
+                    );
+                }
+                ImGui::EndMenu();
+            }
+            ImGui::Separator();
+
+            if(ImGui::BeginMenu("Help##SR"))
+            {
+                if(ImGui::MenuItem("Home page"))
+                {
+                    #ifdef __WINDOWS__
+                    std::system("explorer https://github.com/HenryAWE/SubterraneanRose");
+                    #elif defined __LINUX__
+                    std::system("xdg-open https://github.com/HenryAWE/SubterraneanRose");
+                    #endif
+                }
+                ImGui::Separator();
+                if(ImGui::MenuItem("About"))
+                    show_about = true;
+                ImGui::EndMenu();
+            }
+            ImGui::Separator();
+            ImGui::EndMainMenuBar();
+        }
+
+        if(show_about&&ImGui::Begin("About Subterranean Rose##SR", nullptr, ImGuiWindowFlags_NoCollapse))
+        {
+            if(ImGui::IsItemClicked())
+                show_about = false;
+            ImGui::TextColored(
                 {0, 0.749f, 1, 1},
                 "Subterranean Rose - %d.%d.%d",
                 SR_VERSION_MAJOR, SR_VERSION_MINOR, SR_VERSION_PATCH
             );
-        }
-        ImGui::EndMainMenuBar();
 
-        ImGui::ShowDemoWindow();
+            ImGui::TextWrapped(
+                "2D particle editor and player\n"
+                "Home page: https://github.com/HenryAWE/SubterraneanRose\n\n"
+                "Git commit ID: %I64x",
+                SR_SYS_GitCommitID()
+            );
+            ImGui::End();
+        }
+
+        if(show_imgui_demo)
+            ImGui::ShowDemoWindow();
         SR_WM_EndFrame();
 
         /*Rendering */
