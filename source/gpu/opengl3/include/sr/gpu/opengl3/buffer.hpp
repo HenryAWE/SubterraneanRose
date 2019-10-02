1631b869b0a1543cba0f1d8687816721f23df5e9
AWE Henry <mouth11or12@outlook.com>
2019-10-02 16:39:38 +0800

    OpenGL3: Init module

---
+++ source/gpu/opengl3/include/sr/gpu/opengl3/buffer.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file buffer.hpp
+ * @author HenryAWE
+ * @brief OpenGL buffer manager
+ */
+
+#ifndef SROSE_GPU_OPENGL3_buffer_hpp_
+#define SROSE_GPU_OPENGL3_buffer_hpp_
+
+#include <glad/glad.h>
+
+
+namespace srose::gpu::opengl3
+{
+    class buffer
+    {
+    public:
+        
+    };
+} // namespace srose::gpu::opengl3
+
+
+#endif
---
+++ source/gpu/opengl3/src/buffer.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file buffer.cpp
+ * @author HenryAWE
+ * @brief OpenGL buffer manager
+ */
+
+#include <glad/glad.h>
+#include <sr/gpu/opengl3/buffer.hpp>
+
+
+namespace srose::gpu::opengl3
+{
+    
+} // namespace srose::gpu::opengl3
