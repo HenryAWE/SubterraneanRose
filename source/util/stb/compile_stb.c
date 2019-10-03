407c2e43d6bce4e0ae7faaee4f09a8cc5ebf06f0
AWE Henry <mouth11or12@outlook.com>
2019-10-03 11:23:51 +0800

    Make stb library use UTF-8 on Windows

--- source/util/stb/compile_stb.c
+++ source/util/stb/compile_stb.c
@@ -6,6 +6,8 @@
 
 #define STB_IMAGE_IMPLEMENTATION 1
 #define STBI_ONLY_PNG 1
+#define STBI_WINDOWS_UTF8 1
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION 1
+#define STBIW_WINDOWS_UTF8 1
 #include "stb_image_write.h"
