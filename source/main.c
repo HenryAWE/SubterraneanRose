13713f609015ad889ffcb1be2acaa742c196ec15
AWE Henry <mouth11or12@outlook.com>
2019-10-13 10:06:44 +0800

    subsystem:windows on Windows platform

--- source/main.c
+++ source/main.c
@@ -5,11 +5,36 @@
  */
 
 #include <sr/ui/entry.h>
+#include <SDL_syswm.h>
 
 
 /*Program entry */
-int main(int argc, char* argv[])
+int SDL_main(int argc, char* argv[])
 {
-    //TODO: Handling other platform
-    return program_entry(argc, argv);
+#ifdef __WINDOWS__
+    BOOL attached = AttachConsole(ATTACH_PARENT_PROCESS);
+    if(attached)
+    {
+        FILE* dummy = NULL;
+        freopen_s(&dummy, "conin$", "r+t", stdin);
+        freopen_s(&dummy, "conout$", "w+t", stdout);
+        freopen_s(&dummy, "conout$", "w+t", stderr);
+        printf("\n");
+        if(SetConsoleCP(CP_UTF8) == FALSE)
+            printf("[exec] SetConsoleCP(CP_UTF8) failed, some output may not be displayed correctly\n");
+    }
+#endif
+    int exit_code = program_entry(argc, argv);
+
+#ifdef __WINDOWS__
+    if(attached)
+    {
+        fclose(stdin);
+        fclose(stdout);
+        fclose(stderr);
+        FreeConsole();
+    }
+#endif
+
+    return exit_code;
 }
---
+++ source/subsystem.c
@@ -0,0 +1,9 @@
+/**
+ * @file subsystem.c
+ * @author HenryAWE
+ * @brief Set subsystem on WIN32 to windows
+ */
+
+#ifdef _MSC_VER
+#   pragma comment(linker, "/subsystem:windows")
+#endif
