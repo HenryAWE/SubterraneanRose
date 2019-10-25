88ba97fcb32b7b2299626d44333546c1cbf5d8b7
AWE Henry <mouth11or12@outlook.com>
2019-10-25 18:27:53 +0800

    FS: GetFont() on Windows

--- source/fs/src/fontfile.cpp
+++ source/fs/src/fontfile.cpp
@@ -7,6 +7,11 @@
 #include <sr/filesystem/fontfile.hpp>
 #include <cctype>
 #include <string.h>
+#if defined(_WIN32)||defined(_WIN64)
+#   define WIN32_LEAN_AND_MEAN 1
+#   include <Windows.h>
+#   include <ShlObj.h>
+#endif
 
 
 namespace srose::filesystem
@@ -24,7 +29,7 @@ namespace srose::filesystem
             ext==L".ttc"||ext==L".TTC";
     }
 
-    static std::optional<path> IterateDirectory(const std::string& name, const path& location)
+    static std::optional<path> IterateDirectoryRecursively(const std::string& name, const path& location)
     {
         if(!is_directory(location))
             return std::nullopt;
@@ -43,15 +48,47 @@ namespace srose::filesystem
 
         return std::nullopt;
     }
+    static std::optional<path> IterateDirectory(const std::string& name, const path& location)
+    {
+        if(!is_directory(location))
+            return std::nullopt;
+        for(auto dt : directory_iterator(location))
+        {
+            auto pt = dt.path();
+            if(!is_regular_file(pt) || !pt.has_stem())
+                continue;
+            if(
+                pt.stem().string()==name&&
+                MatchFontFileExt(pt.extension().c_str())
+            ) {
+                return pt;
+            }
+        }
+
+        return std::nullopt;
+    }
 
     std::optional<path> GetFont(const std::string& name)
     {
-        auto pt = IterateDirectory(name, "resource/fonts");
+        auto pt = IterateDirectoryRecursively(name, "resource/fonts");
         if(pt.has_value()) return std::move(pt);
     #ifdef __linux__
-        pt = IterateDirectory(name, "~/.fonts/");
+        pt = IterateDirectoryRecursively(name, "~/.fonts/");
         if(pt.has_value()) return std::move(pt);
-        pt = IterateDirectory(name, "/usr/share/fonts/");
+        pt = IterateDirectoryRecursively(name, "/usr/share/fonts/");
+        if(pt.has_value()) return std::move(pt);
+    #endif
+    #if defined(_WIN32)||defined(_WIN64)
+        wchar_t buffer[MAX_PATH]{};
+        BOOL result = SHGetSpecialFolderPathW(
+            nullptr,
+            buffer,
+            CSIDL_WINDOWS,
+            FALSE
+        );
+        if(result == FALSE)
+            return std::nullopt;
+        pt = IterateDirectory(name, path(buffer)/"Fonts");
         if(pt.has_value()) return std::move(pt);
     #endif
 
