506be0ad151af67c2af31804fa383a821c3ee0c6
AWE Henry <mouth11or12@outlook.com>
2019-11-12 09:34:38 +0000

    UI-UTIL: Update property

--- source/ui/include/sr/ui/property.hpp
+++ source/ui/include/sr/ui/property.hpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <limits>
 #include <algorithm>
+#include <type_traits>
 
 
 namespace srose::ui
@@ -17,8 +18,11 @@ namespace srose::ui
     template <typename T>
     class Property
     {
+        static_assert(std::is_arithmetic_v<T>, "");
     public:
         typedef T value_type;
+        typedef T& reference;
+        typedef const T& const_reference;
 
     private:
         std::string m_name;
@@ -28,17 +32,43 @@ namespace srose::ui
         Property(std::string name,
             value_type init = value_type(),
             value_type min_ = std::numeric_limits<value_type>::min(), value_type max_ = std::numeric_limits<value_type>::max())
-            : m_name(std::move(name)), m_value(init), m_min(min_), m_max(max_) {}
+            : m_name(std::move(name)),
+            m_value(std::move(init)), m_min(std::move(min_)), m_max(std::move(max_)) {}
+
+        [[nodiscard]]
+        constexpr const std::string& name() const noexcept { return m_name; }
 
         [[nodiscard]]
         constexpr value_type min() const noexcept { return m_min; }
-        void min(value_type var) noexcept { m_min = var; }
+        constexpr void min(value_type var) noexcept { m_min = var; }
         [[nodiscard]]
         constexpr value_type max() const noexcept { return m_max; }
-        void max(value_type var) noexcept { m_max = var; }
+        constexpr void max(value_type var) noexcept { m_max = var; }
         [[nodiscard]]
         constexpr value_type value() const noexcept { return m_value; }
-        void value(value_type var) noexcept { std::clamp(var, m_min, m_max); }
+        void value(const_reference var) noexcept { m_value = std::clamp(var, m_min, m_max); }
+    };
+
+    template <>
+    class Property<std::string>
+    {
+    public:
+        typedef std::string value_type;
+
+    private:
+        std::string m_name;
+        std::string m_value;
+
+    public:
+        Property(std::string name, value_type init = "")
+            : m_name(std::move(name)), m_value(std::move(init)) {}
+
+        [[nodiscard]]
+        constexpr const std::string& name() const noexcept { return m_name; }
+
+        [[nodiscard]]
+        constexpr const value_type& value() const noexcept { return m_value; }
+        void value(value_type var) noexcept { m_value = std::move(var); }
     };
 } // namespace srose::ui
 
--- source/ui/test/property_test/ui_property_test.cpp
+++ source/ui/test/property_test/ui_property_test.cpp
@@ -11,7 +11,8 @@ using namespace srose::ui;
 template <typename T> 
 void test_normal()
 {
-    Property<T> pt("test");
+    Property<T> pt("T test");
+    assert(pt.name() == "T test");
     assert(pt.value() == T());
     assert(pt.min() == std::numeric_limits<T>::min());
     assert(pt.max() == std::numeric_limits<T>::max());
@@ -23,12 +24,28 @@ int main()
     assert(pi.value() == 0);
     assert(pi.min() == -15);
     assert(pi.max() == INT_MAX);
+    pi.value(-16); // Will be clamped
+    assert(pi.value() == -15);
+    assert(pi.min() == -15);
+    pi.min(INT_MIN);
+    assert(pi.min() == INT_MIN);
+    pi.value(-100);
+    assert(pi.value() == -100);
 
     Property<float> pf("float", 15.0f);
     assert(std::abs(pf.value() - 15.0f) < FLT_EPSILON);
 
     test_normal<char>();
+    test_normal<char16_t>();
     test_normal<wchar_t>();
+    test_normal<char32_t>();
+    test_normal<unsigned long>();
+
+    Property<std::string> ts("string", "init");
+    assert(ts.name() == "string");
+    assert(ts.value() == "init");
+    ts.value("modified");
+    assert(ts.value() == "modified");
 
     return 0;
 }
