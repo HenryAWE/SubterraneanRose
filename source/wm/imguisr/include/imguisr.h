ab11fbd7958f613c8c93f756b876bb1a329de7f6
AWE Henry <mouth11or12@outlook.com>
2019-11-20 04:16:05 +0000

    ImGuiSR: init

---
+++ source/wm/imguisr/include/imguisr.h
@@ -0,0 +1,19 @@
+/**
+ * @file imguisr.h
+ * @author HenryAWE
+ * @brief ImGui extensions
+ */
+
+#ifndef SROSE_WM_IMGUISR_imguisr_h_
+#define SROSE_WM_IMGUISR_imguisr_h_
+
+#include <imgui.h>
+
+
+namespace ImGuiSR
+{
+    
+} // namespace ImGuiSR
+
+
+#endif
---
+++ source/wm/imguisr/src/imguisr.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file imguisr.cpp
+ * @author HenryAWE
+ * @brief ImGui extensions
+ */
+
+#include <imguisr.h>
+
+
+namespace ImGuiSR
+{
+    
+} // namespace ImGuiSR
