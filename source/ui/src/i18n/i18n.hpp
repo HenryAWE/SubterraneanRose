be697a176985a21d0cfbdbd25b9a0bfc1751d7f3
AWE Henry <mouth11or12@outlook.com>
2020-02-05 22:42:03 +0800

    i18n: Update comments in file 'i18n.hpp'

--- source/ui/src/i18n/i18n.hpp
+++ source/ui/src/i18n/i18n.hpp
@@ -14,9 +14,9 @@
 namespace srose::ui
 {
     /**
-     * @brief Load all language resources
+     * @brief Load all language resources from the locale resource folder
      * 
-     * @param lcres The path to the resources
+     * @param lcres The path to the folder, typically "./locale/"
      */
     void LoadAllLanguage(const std::filesystem::path& lcres);
 
@@ -24,13 +24,14 @@ namespace srose::ui
      * @brief Get the default language
      * 
      * @return locale::Language* The pointer to the default language (never null)
+     * @remark Pointer will be available until program quit
      */
     locale::Language* GetDefaultLanguage() noexcept;
     /**
      * @brief Get the nearest language based on the specific locale
      * 
      * @param lc The specific locale
-     * @return std::shared_ptr<locale::Language> The nearest language, or "C" on error
+     * @return std::shared_ptr<locale::Language> The nearest language, or the default language on error
      */
     std::shared_ptr<locale::Language> GetNearestLanguage(std::locale lc);
 } // namespace srose::ui
