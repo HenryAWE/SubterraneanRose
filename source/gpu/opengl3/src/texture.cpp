06d0e44f323c11b0fb07e123dd41ea93ffa2e09b
AWE Henry <mouth11or12@outlook.com>
2020-02-12 15:47:33 +0800

    OpenGL3: Add glTexParameteri() call for texture loading

--- source/gpu/opengl3/src/texture.cpp
+++ source/gpu/opengl3/src/texture.cpp
@@ -90,6 +90,10 @@ namespace srose::gpu::opengl3
         }
 
         glBindTexture(GL_TEXTURE_2D, m_handle);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexImage2D(
             GL_TEXTURE_2D,
             0,
