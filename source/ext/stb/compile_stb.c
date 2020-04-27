44600811235a830f4473c2ee896acccd92b49c19
AWE Henry <mouth11or12@outlook.com>
2020-04-27 14:57:10 +0800

    Change marco definitions of the stb_image library

--- source/ext/stb/compile_stb.c
+++ source/ext/stb/compile_stb.c
@@ -5,7 +5,10 @@
  */
 
 #define STB_IMAGE_IMPLEMENTATION 1
+#define STBI_ONLY_BMP 1
+#define STBI_ONLY_JPEG 1
 #define STBI_ONLY_PNG 1
+#define STBI_ONLY_TGA 1
 #define STBI_WINDOWS_UTF8 1
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION 1
