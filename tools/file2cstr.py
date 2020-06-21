067ccb618cb443e2bcd64ff3d54bd4f83c9a9908
AWE Henry <mouth11or12@outlook.com>
2020-06-21 17:27:12 +0800

    TOOLS: Update file2cstr.py for output new format

--- tools/file2cstr.py
+++ tools/file2cstr.py
@@ -7,10 +7,26 @@
 
 import sys, argparse, os
 
+def GenerateString(input):
+    output = '"'
+    for i in input:
+        output += "\\x{:02x}".format(i)
+    output += '"'
+    return output
+
+def GenerateArray(input:str):
+    output = "{ "
+    for i in input:
+        output += "0x{:02x},".format(i)
+    output = output[:-1]
+    output += " }"
+    return output
+
 parser = argparse.ArgumentParser(description="File2cstr Options")
 parser.add_argument("--input", "-i", help="Input file(s)", required=True)
 parser.add_argument("--output", "-o", help="Output", required=True)
 parser.add_argument("--cached", help="Cache result", action="store_true")
+parser.add_argument("--format", help="Output format", choices=["string", "array"], default="string")
 
 args = parser.parse_args()
 
@@ -23,7 +39,11 @@ if args.cached and os.path.exists(args.output):
 infile = open(args.input, "rb")
 data = infile.read()
 outfile = open(args.output, "w+")
-outfile.write('"')
-for i in data:
-    outfile.write("\\x{:02x}".format(i))
-outfile.write('"')
+
+written = 0
+if args.format == "string":
+    written = outfile.write(GenerateString(data))
+elif args.format == "array":
+    written = outfile.write(GenerateArray(data))
+
+print("Bytes written: {}".format(written))
