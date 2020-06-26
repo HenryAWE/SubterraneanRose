149bff1bff3171c6730b5fd76241a0b30b2563db
AWE Henry <mouth11or12@outlook.com>
2020-06-26 19:44:25 +0800

    CMAKE&TOOLS: Move some python scripts into scripts/ directory

--- cmake/SRf2c.cmake
+++ cmake/SRf2c.cmake
@@ -1,10 +1,10 @@
 include_guard(DIRECTORY)
 
-set(SROSE_TOOL_DIR ${CMAKE_SOURCE_DIR}/tools)
+set(SROSE_SCRIPTS_DIR ${CMAKE_SOURCE_DIR}/scripts)
 
 # Invoke python script for converting
 function(file2cstr input output)
     execute_process(
-        COMMAND ${PYTHON_EXECUTABLE} "${SROSE_TOOL_DIR}/file2cstr.py" "--input" ${input} "--output" ${output} "--cached"
+        COMMAND ${PYTHON_EXECUTABLE} "${SROSE_SCRIPTS_DIR}/file2cstr.py" "--input" ${input} "--output" ${output} "--cached"
     )
 endfunction()
