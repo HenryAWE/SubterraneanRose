a584c37957ac19ebfb3e6f2b3fecc2c9bc6d5a02
AWE Henry <mouth11or12@outlook.com>
2020-07-07 21:57:12 +0800

    CMAKE: Clean up

--- cmake/SRoseGitInfo.cmake
+++ cmake/SRoseGitInfo.cmake
@@ -4,17 +4,16 @@ include_guard(DIRECTORY)
 find_package(Git QUIET)
 
 
-# Get abbreviated commit id
+# Get log output of git with specific format
 function(sr_git_log output format)
     if(${GIT_FOUND} AND EXISTS "${CMAKE_SOURCE_DIR}/.git")
-        set(${output} TRUE PARENT_SCOPE)
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} log --pretty=format:"${format}" -1
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        OUTPUT_VARIABLE commit_id
-    )
-    # Return value
-    set(${output} ${commit_id} PARENT_SCOPE)
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} log --pretty=format:"${format}" -1
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            OUTPUT_VARIABLE git_output
+        )
+        # Return value
+        set(${output} ${git_output} PARENT_SCOPE)
 
     else()
         set(${output} "\"\"" PARENT_SCOPE)
