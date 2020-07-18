ec5bb8e4857e6b1b21fd715aeeef238aa3bef0a8
AWE Henry <mouth11or12@outlook.com>
2020-07-18 21:51:23 +0800

    CLI: Update

--- source/console/src/cmdline.cpp
+++ source/console/src/cmdline.cpp
@@ -84,25 +84,25 @@ namespace srose::console
 
                 auto _ = [this](std::string_view path) { return language->gettext(path); };
 
-                po::options_description generic(_("srose.cui.generic"), line_length);
+                po::options_description generic(_("srose.cli.generic"), line_length);
                 generic.add_options()
-                    ("help", _("srose.cui.generic.help").c_str())
-                    ("version", _("srose.cui.generic.version").c_str())
-                    ("build-info", _("srose.cui.generic.build").c_str());
+                    ("help", _("srose.cli.generic.help").c_str())
+                    ("version", _("srose.cli.generic.version").c_str())
+                    ("build-info", _("srose.cli.generic.build").c_str());
 
-                po::options_description language(_("srose.cui.lang"), line_length);
+                po::options_description language(_("srose.cli.lang"), line_length);
                 language.add_options()
-                    ("language", po::value<std::string>()->value_name("name")->default_value("auto"), _("srose.cui.lang.language").c_str())
-                    ("available-language", _("srose.cui.lang.available").c_str());
+                    ("language", po::value<std::string>()->value_name("name")->default_value("auto"), _("srose.cli.lang.language").c_str())
+                    ("available-language", _("srose.cli.lang.available").c_str());
 
-                po::options_description display(_("srose.cui.display"), line_length);
+                po::options_description display(_("srose.cli.display"), line_length);
                 display.add_options()
-                    ("fullscreen,F", _("srose.cui.display.fullscreen").c_str())
+                    ("fullscreen,F", _("srose.cli.display.fullscreen").c_str())
                     ("vsync,V", "Enable vertical synchronization");
                 
-                po::options_description video(_("srose.cui.video"), line_length);
+                po::options_description video(_("srose.cli.video"), line_length);
                 video.add_options()
-                    ("get-display-mode", po::value<int>()->value_name("index")->implicit_value(0), _("srose.cui.get-display-mode").c_str());
+                    ("get-display-mode", po::value<int>()->value_name("index")->implicit_value(0), _("srose.cli.get-display-mode").c_str());
 
                 #ifdef __WINDOWS__
                 po::options_description win32("Windows", line_length);
@@ -110,7 +110,7 @@ namespace srose::console
                     ("win-console", po::value<std::string>()->value_name("mode")->default_value("auto"), "Windows console");
                 #endif
 
-                return po::options_description(_("srose.cui.total"), line_length)
+                return po::options_description(_("srose.cli.total"), line_length)
                     .add(generic)
                 #ifdef __WINDOWS__
                     .add(win32)
